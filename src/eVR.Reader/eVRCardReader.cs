50be07b4d6390e8c25db9eab6a064aac1b911aec
eVRMTV <evr@rdw.nl>
2019-06-11 05:59:50 +0200

    Fix to support multiple certificates

--- src/eVR.Reader/eVRCardReader.cs
+++ src/eVR.Reader/eVRCardReader.cs
@@ -71,6 +71,218 @@ namespace EVR.Reader
             set;
         }
 
+        private X509Certificate2[] CSCAList
+        {
+            get;
+            set;
+        }
+
+        public void StopMonitor()
+        {
+            this.cardReader.StopMonitor();
+        }
+
+        public void StartMonitor()
+        {
+            this.cardReader.StartMonitor();
+        }
+
+        
+        public eVRCardReader(X509Certificate2 CSCA, CardRemovedEvent removedEvent, CardInsertedEvent insertedEvent)
+        {
+            TS.TraceI("Constructing MTVCardReader object.");
+            TS.TraceI("eVRCApplicatie = {0}", Helper.ByteArrayToString(eVRCApplicatie));
+            if (CSCA != null)
+            {
+                this.CSCA = CSCA;
+                TS.TraceV("CSCA Subject : \"{0}\".", CSCA.Subject);
+                TS.TraceV("CSCA Effective date : \"{0}\".", CSCA.GetEffectiveDateString());
+                TS.TraceV("CSCA Expiration date : \"{0}\".", CSCA.GetExpirationDateString());
+            }
+            this.cardReader = new CardReader(removedEvent, insertedEvent);
+            TS.TraceI("MTVCardReader constructed.");
+        }
+
+        public eVRCardReader(X509Certificate2[] CSCA, CardRemovedEvent removedEvent, CardInsertedEvent insertedEvent)
+        {
+            TS.TraceI("Constructing MTVCardReader object.");
+            TS.TraceI("eVRCApplicatie = {0}", Helper.ByteArrayToString(eVRCApplicatie));
+            //if (CSCA != null)
+            //{
+                this.CSCAList = CSCA;
+                //TS.TraceV("CSCA Subject : \"{0}\".", CSCA.Subject);
+                //TS.TraceV("CSCA Effective date : \"{0}\".", CSCA.GetEffectiveDateString());
+                //TS.TraceV("CSCA Expiration date : \"{0}\".", CSCA.GetExpirationDateString());
+            //}
+            this.cardReader = new CardReader(removedEvent, insertedEvent);
+            TS.TraceI("MTVCardReader constructed.");
+        }
+
+        public void SelectReader(string readerName)
+        {
+            this.cardReader.SelectReader(readerName);
+            TS.TraceV("Reader \"{0}\" selected.", readerName);
+        }
+
+        public bool CheckATR()
+        {
+            bool result = false;
+
+            TS.TraceI("Start ATR check.");
+            if (string.IsNullOrEmpty(this.cardReader.ReaderName))
+            {
+                throw new eVRCardReaderException("No reader selected.");
+            }
+            byte[] ATR = new byte[] { 0x3B, 0xD2, 0x18, 0x00, 0x81, 0x31, 0xFE, 0x45, 0x01, 0x01, 0xC1 };
+            TS.TraceV("ATR check: {0}", Helper.ByteArrayToString(ATR));
+
+            byte[] response = this.cardReader.GetATRString();
+            TS.TraceV("ATR command response: {0}", Helper.ByteArrayToString(response));
+
+            result = Helper.CompareByteArrays(response, ATR); 
+            
+            TS.TraceI("End ATR check, result: \"{0}\".", result);
+
+            return result;
+        }
+
+        public bool CardManagerDisabled()
+        {
+            bool result = false;
+
+            TS.TraceI("Start CardManagerDisabled check.");
+            if (string.IsNullOrEmpty(this.cardReader.ReaderName))
+            {
+                throw new eVRCardReaderException("No reader selected.");
+            }
+            byte[] cmdSelectCardManager = new byte[] { 0x00, 0xA4, 0x04, 0x00, 0x08, 0xA0, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00 };
+            TS.TraceV("SelectCardManager command: {0}", Helper.ByteArrayToString(cmdSelectCardManager));
+
+            byte[] response = new byte[512];
+
+            this.cardReader.Transmit(cmdSelectCardManager, ref response);
+            TS.TraceV("SelectCardManager response: {0}", Helper.ByteArrayToString(response));
+
+            result = (response.Length == 2 && response[0] == 0x6A && response[1] == 0x82);
+
+            TS.TraceI("End CardManagerDisabled check, result: \"{0}\".", result);
+
+            return (response.Length == 2 && response[0] == 0x6A && response[1] == 0x82);
+        }
+
+        public void Read()
+        {
+            TS.TraceI("Start reading EF data.");
+            if (string.IsNullOrEmpty(this.cardReader.ReaderName))
+            {
+                throw new eVRCardReaderException("No reader selected.");
+            }
+
+            TS.TraceV("Reading EFSod.");
+            try
+            {
+                if (this.CSCAList == null)
+                {
+                    this.EFSOd = new EFSOd(eVRCardReader.eVRCApplicatie, CSCA, this.cardReader);
+                }
+                else
+                {
+                    foreach (var cert in CSCAList)
+                    {
+                        this.EFSOd = new EFSOd(eVRCardReader.eVRCApplicatie, cert, this.cardReader);
+                        if (this.EFSOd.IsValid)
+                        {
+                            System.Console.WriteLine(cert);
+                            this.CSCA = cert;
+                            break;
+                        };
+
+                    }
+                }
+            }
+            catch (ElementaryFileException ex)
+            {
+                throw new eVRCardReaderException("Error reading EFSod.", ex);
+            }
+            TS.TraceV("EFSod read.");
+
+            TS.TraceV("Reading EFAA.");
+            try
+            {
+                this.EFAA = new EFAA(EFSOd, CSCA, eVRCardReader.eVRCApplicatie, this.cardReader);
+            }
+            catch (ElementaryFileException ex)
+            {
+                throw new eVRCardReaderException("Error reading EFAA.", ex);
+            }
+
+            TS.TraceV("Reading EFRegA.");
+            try
+            {
+                this.RegistrationA = new RegistrationA(EFSOd, CSCA, eVRCardReader.eVRCApplicatie, this.cardReader);
+            }
+            catch (ElementaryFileException ex)
+            {
+                throw new eVRCardReaderException("Error reading EFRegA.", ex);
+            }
+            TS.TraceV("EFRegA read.");
+            
+            TS.TraceV("Reading EFRegB.");
+            try
+            {
+                this.RegistrationB = new RegistrationB(EFSOd, CSCA, eVRCardReader.eVRCApplicatie, this.cardReader, this.RegistrationA.CharacterSetEncoding);
+            }
+            catch (ElementaryFileException ex)
+            {
+                throw new eVRCardReaderException("Error reading EFRegB.", ex);
+            }
+            TS.TraceV("EFRegB read.");
+
+            TS.TraceV("Reading EFRegC.");
+            try
+            {
+                this.RegistrationC = new RegistrationC(EFSOd, CSCA, eVRCardReader.eVRCApplicatie, this.cardReader, this.RegistrationA.CharacterSetEncoding);
+            }
+            catch (ElementaryFileException ex)
+            {
+                throw new eVRCardReaderException("Error reading EFRegC.", ex);
+            }
+            TS.TraceV("EFRegC read.");
+
+            TS.TraceI("End reading EF data.");
+        }
+
+        public void Read(string ReaderName)
+        {
+            this.cardReader.SelectReader(ReaderName);
+            this.Read();
+        }
+    }
+}
+        public RegistrationB RegistrationB
+        {
+            get;
+            private set;
+        }
+
+        public RegistrationC RegistrationC
+        {
+            get;
+            private set;
+        }
+
+        private CardReader cardReader
+        {
+            get;
+            set;
+        }
+
+        private X509Certificate2 CSCA
+        {
+            get;
+            set;
+        }
+
         public void StopMonitor()
         {
             this.cardReader.StopMonitor();
